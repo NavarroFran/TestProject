4fd64314962fd36113fabc2a730601177aaeadb3
NavarroFran <navarro.franjav@gmail.com>
2022-04-12 13:07:14 -0300

    Second commit

--- pb2TrabajosPracticos/src/ar/edu/unlam/pb2/tpMonedero/Monedero.java
+++ pb2TrabajosPracticos/src/ar/edu/unlam/pb2/tpMonedero/Monedero.java
@@ -3,5 +3,22 @@ package ar.edu.unlam.pb2.tpMonedero;
 public class Monedero {
 	private Integer dineroActual;
 	
-	public asdas asdas
+	public Monedero (Integer dineroInicial) {
+		this.dineroActual = dineroInicial;
+	}
+	
+	public Integer consultarSaldo() {
+		return this.dineroActual;
+	}
+	
+	public void meterDinero(Integer dineroIngresado) {
+		this.dineroActual += dineroIngresado;
+	}
+	
+	public void retirarDinero(Integer dineroRetirado) {
+		if(dineroRetirado < this.dineroActual) {
+			this.dineroActual -= dineroRetirado;
+		}
+	}
+	
 } 
